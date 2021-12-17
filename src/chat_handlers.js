ead4eb9ab78133c1a62e3385f68413fab21771ef
kamuridesu <myk.gata14@gmail.com>
2021-12-17 12:11:58 -0300

    M - Logger

--- src/chat_handlers.js
+++ src/chat_handlers.js
@@ -1,4 +1,5 @@
 import { Presence } from "@adiwajshing/baileys";
+import { Log } from "../logger/logger.js";
 
 /* USE ESTE ARQUIVO PARA MANIPULAR MENSSAGENS DE TEXTO, NÃO COMANDOS!
 PARA ISSO, CRIE FUNÇÕES PARA CADA MENSSAGEM QUE VOCÊ QUER RESPONDER! 
@@ -7,10 +8,10 @@ ADICIONE SUAS FUNÇÕES NO messageHandler APENAS!*/
 
 async function messageHandler(bot, message, data) {
     // TODO: Adicione suas funções aqui!
-    console.log("Mensagem recebida: " + (message ? message : data.message_data.type));
+    const logger = new Log("./logger/messages.log");
+    logger.write("Mensagem: " + (message ? message : data.message_data.type) + " from " + data.bot_data.sender + (data.bot_data.is_group ? " on group " + data.group_data.name : ""));
     await bot.conn.updatePresence(bot.from, Presence.available);
     if(await getBomDiaMessage(bot, data, message)) {
-        console.log("bon dia")
         return;
     } else if(await getLinkMessage(bot, message, data)) {
         return;
@@ -37,7 +38,7 @@ async function getLinkMessage(bot, message, data) {
     const match = regex.test(message);
     if(match) {
         // sendTextMessageWithMention
-        if(data.bot_data.is_group) {
+        if(data.bot_data.is_group && data.group_data.db_data.anti_link_on) {
             await bot.replyText(data, "É proíbido enviar links aqui! Os admins foram avisados disso!", data.group_data.admins);
             // return bot.replyText("Não pode mandar link!");
             return true;
